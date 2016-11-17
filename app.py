a365bb2118906a674d7452b86e5600fde561bd17
Ubuntu <chammm@gmail.com>
2016-11-17 00:01:38 +0000

    now with sql backend

--- app.py
+++ app.py
@@ -1,29 +1,79 @@
-from bottle import get, post, run, static_file, request
+from bottle import run, get, post, static_file, request
 import random
+from pathlib import Path
+import pymysql
+import sys
+import json
 
 
-annotations = {}
+config = json.load(open("config.json"))
 
 
-@get('/<path:re:css|js|img|boucles_de_garches>/<fname>')
+# Scan all pictures to annotate
+all_photos = [str(p)[3:] for p in Path("../photos/").glob("**/*.jpg")]
+
+print("All photos loaded")
+sys.stdout.flush()
+
+
+def connect():
+    sql = config["sql"]
+    host, user, passwd = sql["host"], sql["user"], sql["passwd"], sql["db"]
+    return pymysql.connect(host=host, user=user, passwd=passwd, db=db, autocommit=False)
+
+
+@get('/<path:re:css|js|img|(photos/.*)>/<fname>')
 @get('/index.html')
 @get('/')
 def serve_css(fname="index.html", path="."):
+    print(path)
+    if path.startswith("photos/"):
+        path = "../" + path
     return static_file(fname, root=path)
 
 
 @get("/random")
 def get_random_pic():
-    imgs = [
-        "boucles_de_garches/14706744_1136003306491793_6299872446280854598_o.jpg",
-        "boucles_de_garches/14714885_1136008809824576_3201558256798966930_o.jpg",
-        "boucles_de_garches/14715461_1136009669824490_7181235540856504290_o.jpg",
-    ]
+    img = random.choice(all_photos)
+    return get_pic(img)
 
-    img = random.choice(imgs)
+
+def annotation_from_row(img, row):
+    text, x, y, width, height = row
+    annotation = {
+	    "img": img,
+	    "text": text,
+	    "box": {
+		    "x": x,
+		    "y": y,
+		    "width": width,
+		    "height": height,
+    }}
+    return annotation
+
+
+@get("/get/<img:path>")
+def get_pic(img):
+    conn = connect()
+    with conn.cursor() as cur:
+        cur.execute("""
+            (SELECT bib, x, y, width, height FROM bibs WHERE img = %s)
+            UNION
+            (SELECT brand, x, y, width, height FROM brands WHERE img = %s)
+            """, (img, img))
+        annotations = [annotation_from_row(img, row) for row in cur.fetchall()]
+    
+        cur.execute("SELECT img FROM annotated ORDER BY datetime DESC LIMIT 5")
+        last_imgs = [r[0] for r in cur.fetchall()]
+
+        cur.execute("SELECT brand, count(*) FROM brands GROUP BY brand ORDER BY count(*) DESC LIMIT 5")
+        best_brands = [(r[0], r[1]) for r in cur.fetchall()]
+
     img = {
         "img": img,
-        "annotations": annotations.get(img),
+        "annotations": annotations,
+        "last_imgs": last_imgs,
+	"best_brands": best_brands,
     }
     return img
 
@@ -31,8 +81,40 @@ def get_random_pic():
 @post("/save")
 def save_and_get_random_pic():
     save = request.json
-    annotations[save["img"]] = save["annotations"]
+    img = save["img"]
+    bibs = []
+    brands = []
+
+    for annotation in save["annotations"]:
+        text = annotation["text"]
+        text = text.lower().strip()
+        box = annotation["box"]
+        record = (img, text, box["x"], box["y"], box["width"], box["height"])
+        if text != "2xu" and text[0].isdigit():
+            bibs.append(record)
+        else:
+            brands.append(record)
+
+    conn = connect()
+    with conn.cursor() as cur:
+        cur.execute("""
+            DELETE annotated, bibs, brands 
+            FROM annotated
+            LEFT JOIN bibs USING(img)
+            LEFT JOIN brands USING(img)
+            WHERE img = %s
+            """, (img, ))
+
+        cur.execute("INSERT INTO annotated (img) VALUES (%s)", (img, ))
+
+        if len(bibs) > 0:
+            print(repr(bibs))
+            cur.executemany("INSERT INTO bibs (img, bib, x, y, width, height) VALUES (%s, %s, %s, %s, %s, %s)", bibs)
+        if len(brands) > 0:
+            cur.executemany("INSERT INTO brands (img, brand, x, y, width, height) VALUES (%s, %s, %s, %s, %s, %s)", brands)
+    conn.commit()
+
     return get_random_pic()
 
 
-run(host='localhost', port=8080)
+run(host='localhost', port=8080, server="cherrypy")
--- js/annotator.js
+++ js/annotator.js
@@ -5,6 +5,8 @@ angular.module('app', [])
     ctrl.img = "/img/gotme.png";
     ctrl.element = document.getElementById('img')
     ctrl.annotations = [];
+    ctrl.last = [];
+    ctrl.brands = [];
 
     ctrl.element.onload = function() {
         anno.reset(ctrl.element)
@@ -16,16 +18,49 @@ angular.module('app', [])
         });
     }
 
+    function processResponse(response) {
+	var my_annotations = [];
+        angular.forEach(response.data.annotations, function(annot) {
+            var my_annotation = {
+                src : response.data.img,
+                text : annot.text,
+                shapes : [{
+                    type : 'rect',
+                    units: 'pixel',
+                    geometry : { 
+                        x : annot.box.x,
+                        y : annot.box.y,
+                        width : annot.box.width,
+                        height : annot.box.height,
+                    }
+                }]
+            };
+            console.log(my_annotation);
+            my_annotations.push(my_annotation);
+        });
+        ctrl.annotations = my_annotations;
+        ctrl.img = response.data.img;
+        ctrl.brands = response.data.best_brands;
+        ctrl.last = response.data.last_imgs;
+    }
+
     ctrl.loadNext = function() {
         $http.get("/random").then(function (response) {
-            ctrl.img = response.data.img;
+	    processResponse(response);
         }, function (error) {
             console.log(error);
             //
         })
     }
 
-    ctrl.loadNext();
+    ctrl.loadImg = function(img) {
+        $http.get("/get/" + img).then(function (response) {
+	    processResponse(response);
+        }, function (error) {
+            console.log(error);
+            //
+        })
+    }
 
     ctrl.saveAndLoadNext = function() {
         var data = anno.getAnnotations();
@@ -52,29 +87,11 @@ angular.module('app', [])
         };
 
         $http.post("/save", my_annotations).then(function (response) {
-            var my_annotations = [];
-            angular.forEach(response.data.annotations, function(annot) {
-                var my_annotation = {
-                    src : response.data.img,
-                    text : annot.text,
-                    shapes : [{
-                        type : 'rect',
-                        units: 'pixel',
-                        geometry : { 
-                            x : annot.box.x,
-                            y : annot.box.y,
-                            width : annot.box.width,
-                            height : annot.box.height,
-                        }
-                    }]
-                };
-                console.log(my_annotation);
-                my_annotations.push(my_annotation);
-            });
-            ctrl.annotations = my_annotations;
-            ctrl.img = response.data.img;
+            processResponse(response);
         }, function (error) {
             console.log(error);
         })
     }
+
+    ctrl.loadNext();
 });
